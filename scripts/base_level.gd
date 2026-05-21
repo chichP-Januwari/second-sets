0fa524a3b97b59efecc06a69175d217384d17ecc
Januwari <217526318+chichP-Januwari@users.noreply.github.com>
2026-05-21 20:52:41 +0800

    phantom camera 2d, final sinatra refactor + extra comments on there

--- scripts/base_level.gd
+++ scripts/base_level.gd
@@ -1,35 +1,26 @@
 extends Node2D
+class_name BaseLevel ## Base level node for Second Set
 
-@export var initial_player_position : Vector2 ## Where the player (Sinatra) spawns.
+# Game State
+enum GAME_STATE {
+	FUCK,
+	SHIT
+}
 
-var sinatra : Node2D
-var sinatra_scene = preload("res://scenes/sinatra.tscn")
-var sinatra_instance = sinatra_scene.instantiate()
-"""
-# var frank : Node2D
-# var frank_scene = preload("res://scenes/frank.tscn")
-# var frank_instance = frank_scene.instantiate()
-"""
+# Nodes
+@onready var sinatra := $Sinatra
+# @onready var frank := $Frank
+@onready var pcam2d := $PhantomCamera2D
 
-var key_collected := false
-
-
+# AI
 var position_array : Array = [] # Contains positions that player (Sinatra) had
 var animstate_array : Array # Contains animation states that player (Sinatra) had
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	add_child(sinatra_instance)
-	sinatra = get_node("Sinatra")
-	sinatra.position = initial_player_position
-	
-	"""
-	add_child(frank_instance)
-	frank = get_node("Frank")
-	sinatra.position = initial_player_position
-	"""
+	pass
 
 
 func _physics_process(_delta: float) -> void:
-	if key_collected == true:
-		position_array.append(position)
+	pass
--- scripts/sinatra_controller.gd
+++ scripts/sinatra_controller.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+# Game State
 enum STATE {
 	IDLE,
 	RUN,
@@ -10,16 +11,27 @@ enum STATE {
 	WALLJUMP,
 }
 
+# Nodes
+@onready var collision: CollisionShape2D = $Collision
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var coyote_timer: Timer = $CoyoteTimer
+@onready var jump_buffer: Timer = $JumpBuffer
+@onready var max_jump_time: Timer = $MaxJumpTime
+@onready var slide_minimum: Timer = $SlideMinimum
+@onready var wall_jump_timer: Timer = $WallJumpTimer
+
+# Speeds
 var walk_velocity := 220.0
 var jump_velocity := -200.0
-var walljump_velocity := -250.0
-var last_walljump := 0.0
-
+var walljump_velocity := -220.0
 var roll_speed := 300.0 # Also used for walljump
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-var active_state := STATE.IDLE
+# Trackers
 var facing_direction := 0 # For animations and sliding, Right
+var last_walljump := 0.0
+var active_state := STATE.IDLE # Default
+
 
 func _physics_process(delta: float) -> void:
 	var direction : float
@@ -32,7 +44,7 @@ func _physics_process(delta: float) -> void:
 		elif sign(direction) == -1:
 			facing_direction = 1 # Left
 	
-	$AnimatedSprite2D.flip_h = facing_direction
+	animated_sprite_2d.flip_h = facing_direction
 	
 	match active_state:
 		STATE.IDLE:
@@ -40,11 +52,11 @@ func _physics_process(delta: float) -> void:
 			if direction: # To RUN
 				switch_state(STATE.RUN) 
 			
-			if Input.is_action_just_pressed("jump") or !$JumpBuffer.is_stopped(): # To JUMP
+			if Input.is_action_just_pressed("jump") or !jump_buffer.is_stopped(): # To JUMP
 				switch_state(STATE.JUMP) 
 			
 			if !is_on_floor(): # To FALL
-				$CoyoteTimer.start()
+				coyote_timer.start()
 				switch_state(STATE.FALL)
 			
 			if Input.is_action_just_pressed("slide"): # To SLIDE
@@ -56,11 +68,11 @@ func _physics_process(delta: float) -> void:
 			if !direction: # To IDLE
 				switch_state(STATE.IDLE) 
 			
-			if Input.is_action_just_pressed("jump") or !$JumpBuffer.is_stopped(): # To JUMP
+			if Input.is_action_just_pressed("jump") or !jump_buffer.is_stopped(): # To JUMP
 				switch_state(STATE.JUMP) 
 			
 			if !is_on_floor(): # To FALL
-				$CoyoteTimer.start()
+				coyote_timer.start()
 				switch_state(STATE.FALL)
 			
 			if Input.is_action_just_pressed("slide"): # To SLIDE
@@ -70,7 +82,7 @@ func _physics_process(delta: float) -> void:
 			velocity.x = direction * walk_velocity
 			velocity.y = jump_velocity
 			
-			if !Input.is_action_pressed("jump") or $MaxJumpTime.is_stopped() or is_on_ceiling_only(): # To FALL
+			if !Input.is_action_pressed("jump") or max_jump_time.is_stopped() or is_on_ceiling_only(): # To FALL
 				switch_state(STATE.FALL)
 			
 		STATE.FALL:
@@ -78,11 +90,11 @@ func _physics_process(delta: float) -> void:
 			velocity.y += gravity * delta
 			
 			if Input.is_action_just_pressed("jump"): # To JUMP
-				if !$CoyoteTimer.is_stopped():
-					$CoyoteTimer.stop()
+				if !coyote_timer.is_stopped():
+					coyote_timer.stop()
 					switch_state(STATE.JUMP)
 				else:
-					$JumpBuffer.start()
+					jump_buffer.start()
 			
 			if is_on_floor(): # To IDLE
 				switch_state(STATE.IDLE)
@@ -97,17 +109,17 @@ func _physics_process(delta: float) -> void:
 				direction = -1
 			velocity.x = roll_speed * direction
 			
-			if $SlideMinimum.is_stopped() or !is_on_floor() or is_on_wall(): # To IDLE
+			if slide_minimum.is_stopped() or !is_on_floor() or is_on_wall(): # To IDLE
 				switch_state(STATE.IDLE)
 			
 		STATE.WALLSLIDE:
 			velocity.x = direction * walk_velocity
 			velocity.y = 30
 			
-			if sign(get_wall_normal().x) == 1:
-				facing_direction = 1
-			elif sign(get_wall_normal().x) == -1:
-				facing_direction = 0
+			if sign(get_wall_normal().x) == 1: # If wall is on left
+				facing_direction = 1 # Face right
+			elif sign(get_wall_normal().x) == -1: # If wall is on right
+				facing_direction = 0 # Face left
 			
 			if !is_on_wall_only(): # To FALL
 				switch_state(STATE.FALL)
@@ -123,14 +135,13 @@ func _physics_process(delta: float) -> void:
 			velocity.x = roll_speed * direction
 			velocity.y = walljump_velocity
 			
-			if !Input.is_action_pressed("jump") or $WallJumpTimer.is_stopped() or is_on_ceiling_only(): # To FALL
+			if !Input.is_action_pressed("jump") or wall_jump_timer.is_stopped() or is_on_ceiling_only(): # To FALL
 				switch_state(STATE.FALL)
 			
 			if direction != last_walljump and is_on_wall_only(): # Charge that motherfucker to the WALL!
 				velocity.x = roll_speed * last_walljump
 				switch_state(STATE.WALLSLIDE)
 	
-	$State.text = STATE.keys()[active_state]
 	move_and_slide()
 
 
@@ -139,33 +150,33 @@ func switch_state(to_state: STATE) -> void: ## Handles switching and animation
 	
 	match active_state:
 		STATE.IDLE:
-			$SlideMinimum.stop()
-			$Collision.shape.size = Vector2(12, 31)
-			$Collision.position = Vector2(0, 0.5)
-			$AnimatedSprite2D.play("sinatra_idle")
+			slide_minimum.stop()
+			collision.shape.size = Vector2(12, 31)
+			collision.position = Vector2(0, 0.5)
+			animated_sprite_2d.play("sinatra_idle")
 			
 		STATE.RUN:
-			$AnimatedSprite2D.play("sinatra_run")
+			animated_sprite_2d.play("sinatra_run")
 			
 		STATE.JUMP:
-			$MaxJumpTime.start()
-			$AnimatedSprite2D.play("sinatra_jump")
+			max_jump_time.start()
+			animated_sprite_2d.play("sinatra_jump")
 			
 		STATE.FALL:
-			$MaxJumpTime.stop()
-			$WallJumpTimer.stop()
-			$AnimatedSprite2D.play("sinatra_fall")
+			max_jump_time.stop()
+			wall_jump_timer.stop()
+			animated_sprite_2d.play("sinatra_fall")
 			
 		STATE.SLIDE:
-			$SlideMinimum.start()
-			$Collision.shape.size = Vector2(12, 7)
-			$Collision.position = Vector2(0, 12.5)
-			$AnimatedSprite2D.play("sinatra_slide")
+			slide_minimum.start()
+			collision.shape.size = Vector2(12, 7)
+			collision.position = Vector2(0, 12.5)
+			animated_sprite_2d.play("sinatra_slide")
 		
 		STATE.WALLSLIDE:
-			$AnimatedSprite2D.play("sinatra_wallslide")
+			animated_sprite_2d.play("sinatra_wallslide")
 		
 		STATE.WALLJUMP:
 			last_walljump = get_wall_normal().x
-			$WallJumpTimer.start()
-			$AnimatedSprite2D.play("sinatra_walljump")
+			wall_jump_timer.start()
+			animated_sprite_2d.play("sinatra_walljump")
